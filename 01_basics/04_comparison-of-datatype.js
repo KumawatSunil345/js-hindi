2fbdd8e352876b71a7e179a1d31e96dec08d57b1
KumawatSunil345 <sunilkumawat2521999@gmail.com>
2024-06-13 03:02:44 +0000

    dataType-summery

---
+++ 01_basics/04_comparison-of-datatype.js
@@ -0,0 +1,22 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// is tarh ke cash me hum avoed hi krte hai(null,undefined) ko !
+
+// ===
+
+console.log("2"===2);
