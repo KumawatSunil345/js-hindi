7f6c974eb5ad4134e9758a3417b52088ee9b9e2b
KumawatSunil345 <sunilkumawat2521999@gmail.com>
2024-06-13 03:03:18 +0000

    dataType-summery

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,29 @@
+//  # primitive datatype
+
+// 7 types : string, Number, Boolearn, Null, undefined, Symbol, BigInt
+
+const score = 100           // Number
+const scoreValue = 100.3   // Number
+
+const isLoggedIn = false     // Boolearn
+const outsideTemp = null    // Null
+
+let userEmail;               // undefined
+let userEmails = undefined  // undefined
+
+const id = Symbol('123')            // Symbol
+const anotherId = Symbol('123')    // Symbol
+
+console.log(id === anotherId);
+
+const bigNumber = 546464546576764644568666n        //bigint
+
+// Reference (Non primitive datatype)
+
+// Array, objects, Functions
+
+const hero = ["shaktiman", "naagraj", "doga"]       // array
+
+{
+    
+}
